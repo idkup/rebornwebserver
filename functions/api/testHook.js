efa91989f9fbbe7e7f9a6ce79f7f5325e77af685
idkup <idkup137@gmail.com>
2024-01-06 01:23:00 -0500

    its a function pls cloudflare

---
+++ functions/api/testHook.js
@@ -0,0 +1,21 @@
+// JavaScript source code
+export async function onRequestPost(ctx){
+	const corsHeaders = {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': '*',
+		'Access-Control-Allow-Headers': '*',
+	};
+	let msg = {};
+	msg.content = 'hahaxd';
+	
+	const response = await fetch(ctx.env.DISCORD_WEBHOOK_URL, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json; charset=utf-8' },
+		body: JSON.stringify(msg)
+	});
+	if (response.status === 200 || response.status === 204) {
+		return new Response('Success', { status: 200, headers: corsHeaders });
+	} else {
+		return new Response('Errored', { status: 500, headers: corsHeaders });
+	}
+}
