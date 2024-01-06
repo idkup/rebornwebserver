add3b7b2a808fc3a78d57f6453c0244037149716
idkup <idkup137@gmail.com>
2024-01-06 01:02:22 -0500

    i forgot a comma

--- functions/testHook.js
+++ functions/testHook.js
@@ -6,7 +6,7 @@ async function testHook(){
 	
 	const response = await fetch(url, {
 		method: 'POST',
-		headers: { 'Content-Type': 'application/json; charset=utf-8' }
+		headers: { 'Content-Type': 'application/json; charset=utf-8' },
 		body: JSON.stringify(msg)
 	});
 }
