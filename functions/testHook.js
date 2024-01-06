588816eb4c3075975cff8df3148d2387662ab858
idkup <idkup137@gmail.com>
2024-01-06 00:47:40 -0500

    webhook test

---
+++ functions/testHook.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+async function testHook(){
+	let url = "https://discord.com/api/webhooks/1193064619175051314/Cbkwg9gRbiJfllr1EOd0zXPuwDTgiCUhfW1KbzO67wsWxbSJYKfkk3BTodyqB8mRbO97";
+	let body = {};
+	body.content = 'hahaxd';
+	
+	const response = await fetch(url, {
+		method: 'POST',
+		headers:'Content-Type': 'application/json; charset=utf-8'
+		body: JSON.stringify(body)
+	});
+}
